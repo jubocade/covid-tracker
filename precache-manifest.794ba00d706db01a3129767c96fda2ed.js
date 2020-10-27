d9791506f3657430c92e18f302dd9875122b8424
Jubo-cade <jubomdivnishvili@gmail.com>
2020-10-27 20:50:36 +0400

    Updates

---
+++ precache-manifest.794ba00d706db01a3129767c96fda2ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5037241a15ebd5a6035986dfeff3292a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "e4bff1afb53701423ec3",
+    "url": "/covid-tracker/static/css/main.25e96a2c.chunk.css"
+  },
+  {
+    "revision": "43d2ee68a868a27450cc",
+    "url": "/covid-tracker/static/js/2.eb3bd9fc.chunk.js"
+  },
+  {
+    "revision": "6bdf6becedb14e4609964580f4c3faf6",
+    "url": "/covid-tracker/static/js/2.eb3bd9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bff1afb53701423ec3",
+    "url": "/covid-tracker/static/js/main.9c78b9cb.chunk.js"
+  },
+  {
+    "revision": "f7dae4dfb137f3b9caa2",
+    "url": "/covid-tracker/static/js/runtime-main.7e9e62f1.js"
+  },
+  {
+    "revision": "7786bbd41b0ab3b8fdc2d472789e306e",
+    "url": "/covid-tracker/static/media/logo.7786bbd4.png"
+  }
+]);
